5e0da1b497ca9908c32015083217e52eddf7f55d
xmk-123 <73211306+xmk-123@users.noreply.github.com>
2024-02-08 13:34:57 -0500

    Update reset.py
    reset.py change

--- libs/reset_device/reset.py
+++ libs/reset_device/reset.py
@@ -1,13 +1,11 @@
-import RPi.GPIO as GPIO
+from gpiozero import Button
 import os
-import time
 import subprocess
 import reset_lib
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+button = Button(4,pull_up=False)
+button.hold_time = 9
 
-counter = 0
 serial_last_four = subprocess.check_output(['cat', '/proc/cpuinfo'])[-5:-1].decode('utf-8')
 config_hash = reset_lib.config_file_hash()
 ssid_prefix = config_hash['ssid_prefix'] + " "
@@ -21,20 +19,5 @@ reboot_required = reset_lib.update_ssid(ssid_prefix, serial_last_four)
 if reboot_required == True:
     os.system('reboot')
 
-# This is the main logic loop waiting for a button to be pressed on GPIO 18 for 10 seconds.
-# If that happens the device will reset to its AP Host mode allowing for reconfiguration on a new network.
 while True:
-    while GPIO.input(18) == 1:
-        time.sleep(1)
-        counter = counter + 1
-
-        print(counter)
-
-        if counter == 9:
-            reset_lib.reset_to_host_mode()
-
-        if GPIO.input(18) == 0:
-            counter = 0
-            break
-
-    time.sleep(1)
+    button.when_held = reset_lib.reset_to_host_mode
